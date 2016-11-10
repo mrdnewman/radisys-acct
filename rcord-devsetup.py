48ff59022c5fed8d342228f9ebea385416267b65
David Newman <dnewman@radisys.com>
2016-11-10 10:52:54 -0800

    modified intro

--- rcord-devsetup.py
+++ rcord-devsetup.py
@@ -1,6 +1,6 @@
 
 
-import lsb_release_ex
+import lsb_release
 import struct
 import os
 import commands
@@ -10,17 +10,19 @@ os.system('clear')
 
 def intro():
 
-        welcome = "\n\n\nRadisy Corp.\nWelcome To Development Server Setup\n\n\n"
+        sup_arch = "x86_64"
+        welcome = "\nRadisy Corp.\nWelcome To Development Server Setup\n"
         get_arch = commands.getoutput("arch")
 
         print '\n'.join('{:^95}'.format(s) for s in welcome.split('\n'))
-        print "Checking Sytem Architecture..."
+        print "Checking System Posture..."
 
-        if get_arch == "x86_64":
+        if get_arch == sup_arch:
             print "Using 64bit OS -- OK\n"
         else:
             print "\nShould be using 64bit OS -- highly recommended"
-            print "Current Arch: %s\n" % get_arch
+            print "Current Arch: %s" % get_arch
+            print "Supported Arch: %s\n" % sup_arch
 
         print "Checking OS Release..."
 
