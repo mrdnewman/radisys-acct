83d2a7f28469a118dc26ae75c1f29c726def14c6
David Newman <dnewman@radisys.com>
2016-11-24 08:30:38 -0800

    constructing additions

---
+++ menu2.onlab.py
@@ -0,0 +1,132 @@
+import netaddr
+import os
+
+os.system('clear')
+
+
+def seed_sub1():
+          print("""
+1. Proceed
+2. Change
+3. Cancel
+            """)
+
+
+def main_select():
+    print("""
+1.Customize Pod Config File
+2.Quit
+      """)
+
+def seed_select():
+          print("""
+1. Enter SeedServer ip
+2. Go back
+               """)
+
+
+def pod_rules():
+
+        config_path = (os.getcwd() + "/cord/config/")
+        print ('''
+=================================================================
+You'll need to customize POD deployment
+by updating the \"onlab_develop_pod.yml\" config file.
+
+You're welcome to do so interactively or
+Just \"Quit\" and manually update...
+
+Location: %s
+
+Please re-run main setup script if manual update
+is chosen.
+=================================================================
+           ''') % config_path
+
+
+def seed_choice():
+    is_valid = 0
+    while not is_valid:
+        try:
+            seed_select()
+            choice = int(raw_input('Enter your choice [1-2] : '))
+            is_valid = 1
+
+        except:
+            print 'Invalid input...'
+        else:
+
+            if choice == 1:
+                seed_action()
+
+            elif choice == 2:
+                onlab_menu()
+
+            elif choice == 3:
+                raise SystemExit
+
+            elif choice > 2:
+                print 'Selection out of range: %d' % choice
+                is_valid = 0
+
+
+def seed_action():
+    done = False
+    while not done:
+
+        seedip = raw_input("SeedServer ip address: ")
+        try:
+            netaddr.ip.IPAddress(seedip)
+            # netaddr.IPNetwork(seedip)
+            done = True
+        except netaddr.core.AddrFormatError:
+            print 'Invalid IP Address Format, please try again!'
+        else:
+            print '\nYou Entered... %s' % seedip
+            is_valid = 0
+
+            while not is_valid:
+                try:
+                    seed_sub1()
+                    choice = int(raw_input('Enter your choice [1-3] : '))
+                    is_valid = 1
+                except:
+                    print 'Invalid input...'
+
+            if choice == 1:
+                print 'Now we go to the next action...'
+            elif choice == 2:
+                seed_action()
+            elif choice == 3:
+                onlab_menu()
+            elif choice > 3:
+                print 'Selection out of range: %d' % choice
+                is_valid = 0
+                seed_action()
+
+
+
+def onlab_menu():
+    is_valid = 0
+    pod_rules()
+    while not is_valid:
+        try:
+            main_select()
+            choice = int(raw_input('Enter your choice [1-2] : '))
+            is_valid = 1
+        except:
+            print 'Invalid input...'
+
+    if choice == 1:
+        seed_choice()
+    elif choice == 2:
+        raise SystemExit
+
+
+
+
+
+
+
+
+onlab_menu()
